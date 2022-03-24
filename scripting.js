cbcf8ca1bf39e61b6c26033103a52422489e83f1
Dom D'Attilio <46716143+ddattilio20@users.noreply.github.com>
2022-03-23 22:44:30 -0500

    Add files via upload

---
+++ scripting.js
@@ -0,0 +1,32 @@
+
+
+function getxRand()
+{
+    return Math.random() * (180 - (-180)) + (-180);
+}
+
+function getyRand()
+{
+    return Math.random() * (90 - (-90)) + (-90);
+}
+
+require([
+    "esri/config",
+     "esri/Map",
+     "esri/views/MapView",
+     "esri/widgets/BasemapToggle",
+     "esri/widgets/BasemapGallery"
+   ], function (esriConfig,Map, MapView, BasemapToggle, BasemapGallery) {
+
+     esriConfig.apiKey = "AAPKd2868e06c350482682380e0457547c6eJL7Rfy_i2OADULBHup8dzZFTRETbH1GuAAQ9Vw7aiyGn8AU2boTDQuMsVjgx8YsS";
+     const map = new Map({
+       basemap: "arcgis-imagery"
+     });
+
+     const view = new MapView({
+       container: "viewDiv",
+       map: map,
+       center: [getxRand(),getyRand()],
+       zoom: 13
+     });
+    });
